c57c624beb24c3f22ab47e7d930f91c83d37347b
mthsgrc <matheus.dev.garcia@gmail.com>
2025-05-09 11:12:27 -0300

    logic to login in user

--- app.py
+++ app.py
@@ -10,10 +10,19 @@ def index():
     words = db.execute("SELECT * FROM words")
     return render_template("index.html", words=words)
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    return    
+    name = request.form.get("login-name")
+    users = db.execute("SELECT username FROM users WHERE user_type_id = 1")
+    if not name:
+        return render_template("error.html", message="Missing user")
+    for user in users:
+        if name not in user["username"]:
+            return render_template("error.html", message="User not in Database")
+        else:
+            return render_template("success.html")
     
+
 @app.route("/filter")
 def filter():
     q = request.args.get("sWord")
